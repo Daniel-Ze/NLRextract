a798513f02f3f94c9e868a1f6bd029db6946b1d6
Daniel <d.zendler@gmx.de>
2020-06-17 11:22:12 +0200

    .

--- plotNLR.r
+++ plotNLR.r
@@ -7,12 +7,11 @@ nlrs <- commandArgs( )
 
 # Extracting the file path
 nlrs_file <- nlrs[6]
-
 # Extracting directory and file name from path
 nlrs_file_path <- dirname( nlrs_file )
 nlrs_file_name <- basename( nlrs_file )
 
-print( paste0( "Plotting data from ", nlrs_file_name ) )
+print( paste0( "# Plotting data from ", nlrs_file_name ) )
 
 # Reading the data as table with tab stop as delimiter
 nlrs_tbl <- read.table( nlrs_file,
@@ -38,8 +37,8 @@ nlr_comp$V2 <- factor ( nlr_comp$V2,
                                     "NBARC_TIR_LRR.protein",
                                     "NBARC_CC_RPW8_LRR.protein",
                                     "NBARC_RPW8_LRR.protein"
-                        )
-)
+                                  )
+                      )
 
 #Partial NLRs
 nlr_cc_nb <- nlrs_tbl[grepl("^NBARC_CC.protein$", nlrs_tbl$V2), ]
@@ -56,27 +55,29 @@ nlr_rpw8 <- nlrs_tbl[grepl("^RPW8.protein$", nlrs_tbl$V2), ]
 nlr_part <- rbind(nlr_cc_nb,nlr_tir_nb,nlr_rpw8_nb,nlr_nb_lrr,nlr_cc_lrr,nlr_tir_lrr,nlr_nb,nlr_cc,nlr_tir,nlr_rpw8)
 
 nlr_part$V2 <- factor ( nlr_part$V2,
-                        levels = c( "NBARC_CC.protein",
+                        levels = c( "NBARC_LRR.protein",
+                                    "NBARC_CC.protein",
                                     "NBARC_TIR.protein",
                                     "NBARC_RPW8.protein",
-                                    "NBARC_LRR.protein",
                                     "CC_LRR.protein",
                                     "TIR_LRR.protein",
                                     "NBARC.protein",
-                                    "CC.protein",
                                     "TIR.protein",
+                                    "CC.protein",
                                     "RPW8.protein"
-                        )
-)
+                                  )
+                      )
 
 nlr_dom$V2 <- factor( nlr_dom$V2,
-                      levels = c( "NB-ARC",
-                                  "CC",
-                                  "TIR",
-                                  "RPW8",
-                                  "LRR"
-                      )
-)
+                       levels = c( "NB-ARC",
+                                   "CC",
+                                   "TIR",
+                                   "RPW8",
+                                   "LRR"
+                                 )
+                    )
+
+
 
 # Generating bar plot of data from NLRextract
 pdf(paste0(nlrs_file_path,"/nlr.domain.pdf"))
@@ -84,37 +85,41 @@ ggplot( nlr_dom,
         aes( nlr_dom$V2,
              nlr_dom$V1,
              fill = nlr_dom$V2
-        )
-)+
-  geom_bar( stat = "identity" )+
+            )
+      )+
+  geom_bar( stat = "identity",
+            show.legend=FALSE
+          )+
   ylab( "# NLR" )+
   theme_bw()+
   theme( axis.title.x = element_blank(),
          axis.text.x = element_text( angle = 45,
                                      hjust = 1
-         ),
+                                    ),
          legend.title = element_blank()
-  )+
+        )+
   ggtitle( "NLR domains" )
 dev.off()
 
 # Generating bar plot of data from NLRextract
 pdf(paste0(nlrs_file_path,"/nlr.protein.pdf"))
 ggplot( nlr_prot,
-        aes( nlr_prot$V2,
-             nlr_prot$V1,
-             fill = nlr_prot$V2
-        )
-)+
-  geom_bar( stat = "identity" )+
+        aes( x=reorder(V2, -V1),
+             y=V1,
+             fill = V2
+            )
+      )+
+  geom_bar( stat = "identity",
+            show.legend=FALSE
+          )+
   ylab( "# proteins" )+
   theme_bw()+
   theme( axis.title.x = element_blank(),
          axis.text.x = element_text( angle = 45,
                                      hjust = 1
-         ),
+                                    ),
          legend.title = element_blank()
-  )+
+        )+
   ggtitle( "NLR proteins" )
 dev.off()
 
@@ -124,8 +129,10 @@ ggplot( nlr_comp,
              nlr_comp$V1,
              fill = nlr_comp$V2
         )
-)+
-  geom_bar( stat = "identity" )+
+      )+
+  geom_bar( stat = "identity",
+            show.legend=FALSE
+          )+
   ylab( "# proteins" )+
   theme_bw()+
   theme( axis.title.x = element_blank(),
@@ -133,7 +140,7 @@ ggplot( nlr_comp,
                                      hjust = 1
          ),
          legend.title = element_blank()
-  )+
+       )+
   ggtitle( "NLR complete proteins" )
 dev.off()
 
@@ -143,8 +150,10 @@ ggplot( nlr_part,
              nlr_part$V1,
              fill = nlr_part$V2
         )
-)+
-  geom_bar( stat = "identity" )+
+      )+
+  geom_bar( stat = "identity",
+            show.legend=FALSE
+          )+
   ylab( "# proteins" )+
   theme_bw()+
   theme( axis.title.x = element_blank(),
@@ -152,7 +161,7 @@ ggplot( nlr_part,
                                      hjust = 1
          ),
          legend.title = element_blank()
-  )+
+       )+
   ggtitle( "NLR - partial proteins" )
 dev.off()
 
