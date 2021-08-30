394cf57b53e70273d9e7e9a3853eda3818e7dee1
Daniel-Ze <d.zendler@gmx.de>
2021-08-30 15:00:01 +0200

    Update plotNLR.r

--- plotNLR.r
+++ plotNLR.r
@@ -1,5 +1,4 @@
 #!/usr/bin/Rscript
-#install.packages("ggplot2")
 library(ggplot2)
 library(egg)
 library(cowplot)
@@ -42,12 +41,16 @@ nlr_comp$V2 <- factor ( nlr_comp$V2,
                                   )
                       )
 
+#MADA
+nlr_cc_mada <- nlrs_tbl[grep("MADA", nlrs_tbl$V2), ] 
+print(nlr_cc_mada)
+
 #Partial NLRs
 nlr_cc_nb <- nlrs_tbl[grepl("^NBARC_CC.protein$", nlrs_tbl$V2), ]
 nlr_tir_nb <- nlrs_tbl[grepl("^NBARC_TIR.protein$", nlrs_tbl$V2), ]
 nlr_rpw8_nb <- nlrs_tbl[grepl("^NBARC_RPW8.protein$", nlrs_tbl$V2), ]
 nlr_nb_lrr <- nlrs_tbl[grepl("^NBARC_LRR.protein$", nlrs_tbl$V2), ]
-nlr_cc_lrr <- nlrs_tbl[grep("^CC_LRR.protein$", nlrs_tbl$V2), ]
+nlr_cc_lrr <- nlrs_tbl[grepl("^CC_LRR.protein$", nlrs_tbl$V2), ]
 nlr_tir_lrr <- nlrs_tbl[grepl("^TIR_LRR.protein$", nlrs_tbl$V2), ]
 nlr_nb <- nlrs_tbl[grepl("^NBARC.protein$", nlrs_tbl$V2), ]
 nlr_cc <- nlrs_tbl[grepl("^CC.protein$", nlrs_tbl$V2), ]
@@ -193,6 +196,33 @@ p4<-ggplot( nlr_part,
        )+
   ggtitle( "NLR - partial proteins" )
 
+p6<-ggplot( nlr_cc_mada,
+            aes( reorder(nlr_cc_mada$V2, -nlr_cc_mada$V1),
+                 nlr_cc_mada$V1,
+                 fill = nlr_cc_mada$V2
+               )
+          )+
+  geom_bar( stat = "identity",
+            show.legend=FALSE,
+            alpha = 0.7,
+            color = "black",
+            width = 0.8
+          )+
+  geom_text( aes(label = V1), 
+             vjust = -0.5,
+             show.legend = FALSE 
+           )+
+  ylab( "# proteins" )+
+  theme_bw()+
+  theme( axis.title.x = element_blank(),
+         axis.title.y = element_blank(),
+         axis.text.x = element_text( angle = 90,
+                                     hjust = 1,                                                                                                 vjust = 0.5,
+                                     size = 12),
+                     legend.title = element_blank()
+       )+
+  ggtitle( "NLR - CC with MADA" )
+
 
 # dev.off()
 p5<-plot_grid(p3,p4, nrow = 1, align = "h", rel_widths = c(2.5,4))
@@ -207,4 +237,6 @@ ggsave(file = paste0(nlrs_file_path, "/nlr.protein.part.pdf"),
        width = 5, height = 7, p4)
 ggsave(file = paste0(nlrs_file_path, "/nlr.protein.comp_part.pdf"),
        width = 8, height = 7, p5)
+ggsave(file = paste0(nlrs_file_path, "/nlr.protein.cc_mada.pdf"),
+              width = 5, height = 7, p6)
 print( paste0( "Done." ) )
